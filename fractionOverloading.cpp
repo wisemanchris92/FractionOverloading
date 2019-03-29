b12e60013d2744790977dbf4e7503f1675839394
Chris Wiseman <wisemanchris92@gmail.com>
2019-03-29 12:30:31 -0400

    first commit

---
+++ fractionOverloading.cpp
@@ -0,0 +1,8 @@
+/* Creator:		Chris Wiseman
+ * Program name:	fractionOverloading
+ * Description:		A program that adds, subtracts, multiplies and divides a
+ *			fraction using QString, QTextString and overloaded operators.
+ * Last Modification:	29MAR19
+ * ==================================================================================
+ */
+
